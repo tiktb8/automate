d37e04f145cc33f866ab07fd4cfdb00211c76dd2
Jonas Vachal <jonas@vertexcg.com>
2018-10-20 14:20:12 -0600

    Fixed invalid call to omreport

--- src/Backup Management/Azure Site Recovery Services Agent/Monitors/Get-LastBackupJobStatus.ps1
+++ src/Backup Management/Azure Site Recovery Services Agent/Monitors/Get-LastBackupJobStatus.ps1
@@ -1,5 +1,5 @@
 # Remote Monitor Name: BU - Azure Recovery Services Agent - Last Backup Status 
-# Remote Monitor Command: "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -noprofile -command "(new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tiktb8/automate/master/Backup Management/Azure Site Recovery Services Agent/Get-LastBackupJobStatus.ps1') | iex;"
+# Remote Monitor Command: "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -noprofile -command "(new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tiktb8/automate/master/src/Backup Management/Azure Site Recovery Services Agent/Get-LastBackupJobStatus.ps1') | iex;"
 Import-Module MSOnlineBackup
 $lastJob = Get-OBJob -Previous 1
 if ($lastJob.JobStatus.JobState -ne 'Completed')
--- src/Hardware Manufacturers/Dell/Servers/Monitors/Get-DellChassisStatus.ps1
+++ src/Hardware Manufacturers/Dell/Servers/Monitors/Get-DellChassisStatus.ps1
@@ -5,7 +5,7 @@
 # 3 - UNKNOWN
 # Remote Monitor Name: HW - Dell Server Chassis Status 
 # Remote Monitor Command: "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -noprofile -command "(new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tiktb8/automate/master/src/Hardware Manufacturers/Dell/Servers/Monitors/Get-DellChassisStatus.ps1') | iex;"
-$chassisStatus = omstatus chassis | out-string
+$chassisStatus = omreport chassis | out-string
 if($chassisStatus -Match "Critical")
 {
     return 2
