4da3ca4e877bb3cf781c17db153bba164ee957e6
Jonas Vachal <jonas@vertexcg.com>
2018-10-20 14:14:05 -0600

    Added Dell Chassis Monitor and Last Backup Job Monitor

---
+++ src/Backup Management/Azure Site Recovery Services Agent/Monitors/Get-LastBackupJobStatus.ps1
@@ -0,0 +1,13 @@
+# Remote Monitor Name: BU - Azure Recovery Services Agent - Last Backup Status 
+# Remote Monitor Command: "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -noprofile -command "(new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tiktb8/automate/master/Backup Management/Azure Site Recovery Services Agent/Get-LastBackupJobStatus.ps1') | iex;"
+Import-Module MSOnlineBackup
+$lastJob = Get-OBJob -Previous 1
+if ($lastJob.JobStatus.JobState -ne 'Completed')
+{
+    return 2
+}
+else
+{
+    return 0
+}
+    
