ffd44b232f9b23ee0997ce92e26ecf000a1fc06b
Jonas Vachal <jonas@vertexcg.com>
2018-10-20 15:00:56 -0600

    Hide the results of omconfig preferences in a variable

--- src/Hardware Manufacturers/Dell/Servers/Monitors/Get-DellVirtualDiskStatus.ps1
+++ src/Hardware Manufacturers/Dell/Servers/Monitors/Get-DellVirtualDiskStatus.ps1
@@ -5,7 +5,7 @@
 # 3 - UNKNOWN
 # Remote Monitor Name: HW - Dell Server Virtual Disk Status 
 # Remote Monitor Command: "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe" -noprofile -command "(new-object Net.WebClient).DownloadString('https://raw.githubusercontent.com/tiktb8/automate/master/src/Hardware Manufacturers/Dell/Servers/Monitors/Get-DellVirtualDiskStatus.ps1') | iex;"
-omconfig preferences cdvformat delimiter=comma
+$preferenceStatus = omconfig preferences cdvformat delimiter=comma
 $OmReport = omreport storage vdisk -fmt cdv |  select-string -SimpleMatch "ID,Status," -Context 0,5000
 
 $vdisks = ConvertFrom-Csv $OmReport -Delimiter ","
