b593e53ca711d825c59a4694ad34ae7cadeea267
jdvachal <jonas@vertexcg.com>
2019-08-23 09:46:40 -0600

    Ignore Controllers With No Battery
    Ignore Controllers With No Battery

--- src/Hardware Manufacturers/HP/Servers/Monitors/Get-HPArrayControllerBatteryStatus.ps1
+++ src/Hardware Manufacturers/HP/Servers/Monitors/Get-HPArrayControllerBatteryStatus.ps1
@@ -16,6 +16,11 @@ try {
     $arraySystems =  Get-WmiObject -Namespace root\hpq -Class HPSA_ArraySystem
     ForEach ($arraySystem in $arraySystems){
         $arrayController = Get-WmiObject -Namespace root\hpq -Query ("associators of {HPSA_ArraySystem.CreationClassName='HPSA_ArraySystem',Name='" + $arraySystem.Name + "'} WHERE AssocClass=HPSA_ArraySystemArrayController")
+        if ($arrayController.BatteryStatus -eq $null)
+        {
+            #No Battery
+            return 0
+        }
                 
         
         if ($arrayController.BatteryStatus -ne 1)
